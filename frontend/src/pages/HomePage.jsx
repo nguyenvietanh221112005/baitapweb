b1fb3433d3450fcbc1b9d72d632a0aa42686f82d
nguyenvietanh221112005 <165276644+nguyenvietanh221112005@users.noreply.github.com>
2024-12-24 22:08:44 +0700

    Update HomePage.jsx

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -11,9 +11,9 @@ function HomePage() {
 
   // Dữ liệu banner
   const banners = [
-    'https://cebcu.com/wp-content/uploads/2024/09/anh-gai-xinh-choi-pickleball-1.webp',
-    'https://kenh14cdn.com/203336854389633024/2024/8/24/6214988364063555428-1724411632790541174182-1724416058529-1724416059139743604134-1724467772591-17244677729041755822096.jpg',
-    'https://kenh14cdn.com/thumb_w/660/203336854389633024/2024/8/16/k14-33-17238061784712061040329.jpg'
+    'https://shopvnb.com/uploads/images/tin_tuc/kich-thuoc-san-pickleball-3-1717631144.webp',
+    'https://sonsanepoxy.vn/wp-content/uploads/2024/07/Thi-c%C3%B4ng-s%C3%A2n-Pickleball-11.png',
+    'https://sukavietnam.com/wp-content/uploads/2024/09/sukavietnam.com-hinh-anh-mau-san-pickleball-dep-1.webp'
   ];
 
   useEffect(() => {
